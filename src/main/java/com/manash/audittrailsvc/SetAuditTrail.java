a5cd1476cd3a04b11c145fc5ddadbff8da76a427
manash31@gmail.com <manash31@gmail.com>
2023-03-14 01:24:43 -0400

    Initial commit

---
+++ src/main/java/com/manash/audittrailsvc/SetAuditTrail.java
@@ -0,0 +1,53 @@
+package com.manash.audittrailsvc;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+
+public class SetAuditTrail implements JavaDelegate {
+    private final Logger logger = LoggerFactory.getLogger(SetAuditTrail.class);
+
+    Properties props;
+
+    public void execute(DelegateExecution execution) throws Exception{
+
+        String content = (String) execution.getVariable("content");
+        if(content.equals("")){
+            throw new RuntimeException("No Content found to publish");
+        }
+
+        String TopicName = "Camunda-AuditTrail-events";// (String) execution.getVariable("Camunda-AuditTrail-events");
+        if(TopicName.equals("")){
+            throw new RuntimeException("No Topic found to publish");
+        }
+        logger.info("TopicName :"+TopicName);
+        logger.info("content :"+content);
+
+        props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 0);
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        producer.send(new ProducerRecord<String, String>(TopicName, content,content ));
+        logger.info("Message sent successfully to Topic"+TopicName+" content"+content);
+        producer.close();
+
+    }
+}
